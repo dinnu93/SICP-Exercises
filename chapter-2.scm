c08de0f47ec4a211e49dc735a8df4475bc6712e7
Dinesh Padisetti <dinesh.padisetti@gmail.com>
2015-11-16 01:29:27 +0530

    Added chapter-2 exercise code

--- chapter-2.scm
+++ chapter-2.scm
@@ -1,4 +1,4 @@
-;; Rational numbers data abstraction implementation
+;;; Rational numbers data abstraction implementation (Constructor)
 
 (define (make-rat num den)
   (let ((g (gcd num den)))
@@ -6,7 +6,7 @@
       ((< den 0) (cons (/ (* -1 num) g) (/ (* -1 den) g)))
       (else (cons (/  num g) (/ den g))))))
 
-;; Operations on the rational numbers data type
+;; Operations on the rational numbers data type (Selectors)
 
 (define (numer x)
   (car x))
@@ -39,3 +39,44 @@
 (define (equal-rat? x y)
   (= (* (numer x) (denom y))
      (* (numer y) (denom x))))
+
+;;; Representing points in the space as data (Constructor)
+
+(define (make-point x y)
+  (cons x y))
+
+;; Operations on points (Selectors)
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (print-point p)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+
+;;; Representing line segments with points (Constructor)
+
+(define (make-segment p1 p2)
+  (cons p1 p2))
+
+;; Operations on segments (Selectors)
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+(define (midpoint-segment segment)
+  (make-point (/ (+ (x-point (start-segment segment)) (x-point (end-segment segment))) 2)
+              (/ (+ (y-point (start-segment segment)) (y-point (end-segment segment))) 2)))
+
+
+
