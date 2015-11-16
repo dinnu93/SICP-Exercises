d0050e42f98948ccc3bb66cd9f08330c5baf7bde
Dinesh Padisetti <dinesh.padisetti@gmail.com>
2015-11-16 20:14:12 +0530

    Added upto exercise 2.6

--- chapter-2.scm
+++ chapter-2.scm
@@ -60,6 +60,12 @@
   (display (y-point p))
   (display ")"))
 
+(define (square x)
+  (* x x))
+
+(define (distance-points p1 p2)
+  (sqrt (+ (square (- (x-point p1) (x-point p2)))
+           (square (- (y-point p1) (y-point p2))))))
 
 ;;; Representing line segments with points (Constructor)
 
@@ -78,5 +84,64 @@
   (make-point (/ (+ (x-point (start-segment segment)) (x-point (end-segment segment))) 2)
               (/ (+ (y-point (start-segment segment)) (y-point (end-segment segment))) 2)))
 
+(define (length-of-segment segment)
+  (distance-points (start-segment segment) (end-segment segment)))
 
+;;; Representing rectangles with two perpendicular line segments
 
+(define (make-rectangle s1 s2)
+  (cons s1 s2))
+
+(define (length-segment rect)
+  (car rect))
+
+(define (height-segment rect)
+  (cdr rect))
+
+;; Operations on rectangle
+
+(define (rectangle-area rect)
+  (* (length-of-segment (length-segment rect)) (length-of-segment (height-segment rect))))
+  
+(define (rectangle-perimeter rect)
+  (* 2 (+ (length-of-segment (length-segment rect)) (length-of-segment (height-segment rect)))))
+
+;; Define Rectangle
+
+(define p1 (make-point 2 6))
+(define p2 (make-point 2 3))
+(define p3 (make-point 7 3))
+
+(define s1 (make-segment p1 p2))
+(define s2 (make-segment p2 p3))
+
+(define rect (make-rectangle s1 s2))
+
+;;; Defining cons as a procedure (Alternative)
+
+(define (cons/alt x y)
+  (lambda (m) (m x y)))
+
+(define (car/alt z)
+  (z (lambda (p q) p)))
+
+(define (cdr/alt z)
+  (z (lambda (p q) q)))
+
+;;; pair of non-negative numbers as 2^a * 3^b
+(define (cons/int a b)
+  (* (expt 2 a) (expt 3 b)))
+
+(define (highest-power-of-n a n)
+  (if (= (remainder a n) 0)
+      (+ 1 (highest-power-of-n (/ a n) n))
+      0
+      ))
+
+(define (car/int c)
+  (highest-power-of-n c 2))
+
+(define (cdr/int c)
+  (highest-power-of-n c 3))
+
+;;; 
